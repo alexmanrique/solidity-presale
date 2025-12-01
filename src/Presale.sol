72e666a9524c45da24bde3efe6a6e43f1b47ca7c
alex <amanrique86@gmail.com>
2025-12-01 16:20:01 +0100

    add more functions to presale contract

--- src/Presale.sol
+++ src/Presale.sol
@@ -5,6 +5,7 @@ import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.so
 import {SafeERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IAggregator} from "./interfaces/IAggregator.sol";
 
 contract Presale is Ownable {
     using SafeERC20 for IERC20;
@@ -18,11 +19,14 @@ contract Presale is Ownable {
     uint256 public startingTime;
     uint256 public endingTime;
     uint256 public currentPhase;
+    address public dataFeedAddress;
+    address public saleTokenAddress;
 
     mapping(address => bool) public isBlackListed;
     mapping(address => uint256) public userTokenBalance;
 
     event TokenBuy(address user, uint256 amount);
+    event TokensClaimed(address user, uint256 amount);
 
     constructor(
         address usdtAddress_,
@@ -31,7 +35,9 @@ contract Presale is Ownable {
         uint256 maxSellingAmount_,
         uint256[][3] memory phases_,
         uint256 startingTime_,
-        uint256 endingTime_
+        uint256 endingTime_,
+        address dataFeedAddress_,
+        address saleTokenAddress_
     ) Ownable(msg.sender) {
         usdtAddress = usdtAddress_;
         usdcAddress = usdcAddress_;
@@ -40,8 +46,12 @@ contract Presale is Ownable {
         phases = phases_;
         startingTime = startingTime_;
         endingTime = endingTime_;
+        dataFeedAddress = dataFeedAddress_;
+        saleTokenAddress = saleTokenAddress_;
 
         require(startingTime < endingTime, "Starting time must be before ending time");
+
+        IERC20(saleTokenAddress).safeTransferFrom(msg.sender, address(this), maxSellingAmount);
     }
 
     function blackList(address user) external onlyOwner {
@@ -73,7 +83,46 @@ contract Presale is Ownable {
 
         IERC20(tokenUsedToBuy_).safeTransferFrom(msg.sender, fundsReceiverAddress, amount_);
 
-        emit TokenBuy(msg.sender, amount_);
+        emit TokenBuy(msg.sender, tokenAmountToReceive);
+    }
+
+    function buyWithEth() external payable {
+        require(!isBlackListed[msg.sender], "You are blacklisted");
+        require(block.timestamp >= startingTime && block.timestamp <= endingTime, "Presale is not active");
+
+        uint256 tokenAmountToReceive;
+
+        uint256 usdValue = msg.value * getEtherPrice() / 1e18;
+
+        tokenAmountToReceive = usdValue * 1e6 / phases[currentPhase][1];
+
+        checkCurrentPhase(tokenAmountToReceive);
+
+        totalAmountSold += tokenAmountToReceive;
+        require(totalAmountSold <= maxSellingAmount, "Sold out");
+
+        userTokenBalance[msg.sender] += tokenAmountToReceive;
+
+        (bool success,) = fundsReceiverAddress.call{value: msg.value}("");
+        require(success, "ETH transfer failed");
+
+        emit TokenBuy(msg.sender, tokenAmountToReceive);
+    }
+
+    function getEtherPrice() public view returns (uint256) {
+        (, int256 price,,,) = IAggregator(dataFeedAddress).latestRoundData();
+        price = price * (10 ** 10);
+        return uint256(price);
+    }
+
+    function claimTokens() external {
+        require(userTokenBalance[msg.sender] > 0, "No tokens to claim");
+        require(block.timestamp > endingTime, "Presale is not over");
+
+        delete userTokenBalance[msg.sender];
+        IERC20(saleTokenAddress).safeTransfer(msg.sender, userTokenBalance[msg.sender]);
+
+        emit TokensClaimed(msg.sender, userTokenBalance[msg.sender]);
     }
 
     function emergencyWithdraw(address tokenAddress_, uint256 amount_) external onlyOwner {
